c7e9c69b5d3c6581d36c07904e77e4b539d4a28d
kimyena1123 <kyq104@gmail.com>
2024-03-22 01:00:20 +0900

    내 입력값이 유효하지 않을 때 적용

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,17 @@ export default function Home() {
           2xl:bg-purple-100">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2 md:flex-row">
         {/* input과 button의 컨테이너 역할을 하는 카드 컴포넌트를 만들기 */}
-        <input className="w-full rounded-full h-12 
-                        bg-gray-200 pl-5 outline-none ring ring-transparent
-                        focus:ring-orange-500
-                        focus:ring-offset-2 transition-shadow
-                        placeholder:drop-shadow"
-               type="text" 
-               placeholder="search here.." 
+        <input className="w-full rounded-full h-10 bg-gray-200 pl-5
+                          outline-none ring ring-transparent focus:ring-green-500
+                          focus:ring-offset-2 transition-shadow
+                          placeholder:drop-shadow
+                          invalid:focus:ring-red-700"
+               type="email" 
+               placeholder="search here.."
+               required 
         />
         {/* 그라데이션 적용 */}
-        <button className="bg-gradient-to-tr from-cyan-500 via-yellow-400 to-purple-400 
+        <button className="bg-black
                          text-white py-2 rounded-full active:scale-90 
                           transition-transform font-medium focus:scale-90 outline-none
                           md:px-10"> {/* 반응형 적용 */}
@@ -31,4 +32,4 @@ export default function Home() {
 }
 
 // 폼을 만들 때 사용할 수 있는 몇가지 modifier
-// Tailwind CSS에서 그라데이션 배경을 만드는 게 쉽다
+//내 입력값이 유효하지 않다면 배경색을 red-100으로 변경
