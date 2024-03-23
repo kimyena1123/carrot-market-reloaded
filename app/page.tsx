76f748a5bd94dc997791b48ad3e9110689347569
kimyena1123 <kyq104@gmail.com>
2024-03-23 15:05:24 +0900

    peer and has

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,21 @@ export default function Home() {
           lg:bg-cyan-100
           xl:bg-orange-100
           2xl:bg-purple-100">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2 md:flex-row">
+      <div className="bg-white shadow-lg p-5 
+                      rounded-3xl w-full max-w-screen-sm flex flex-col gap-2 
+                      md:flex-row
+                      *:outline-none
+                      ring ring-transparent transition-shadow
+                      has-[:invalid]:ring-red-100
+                     ">{/* *:md:text-pink-900 */}
         {/* input과 button의 컨테이너 역할을 하는 카드 컴포넌트를 만들기 */}
         <input className="w-full rounded-full h-10 bg-gray-200 pl-5
-                          outline-none ring ring-transparent focus:ring-green-500
+                          ring ring-transparent focus:ring-green-500
                           focus:ring-offset-2 transition-shadow
                           placeholder:drop-shadow
                           invalid:focus:ring-red-700
                           peer"
-               type="email" 
+               type="text" 
                placeholder="Email address"
                required 
         />
@@ -26,9 +32,9 @@ export default function Home() {
         {/* 그라데이션 적용 */}
         <button className="bg-black
                          text-white py-2 rounded-full active:scale-90 
-                          transition-transform font-medium focus:scale-90 outline-none
+                          transition-transform font-medium focus:scale-90
                           md:px-10
-                          peer-invalid:bg-red-100"> {/* peer-required:bg-green-500*/}
+                         "> {/* peer-invalid:bg-red-100" , peer-required:bg-green-500*/}
           Log in
         </button>
       </div>
@@ -36,5 +42,13 @@ export default function Home() {
   );
 }
 
-// 폼을 만들 때 사용할 수 있는 몇가지 modifier
-// peer
+
+// 요소의 컨테이너에 적용할 수 있는 것들
+// 형제 : peer
+// 자식 : *
+// has : 해당 컨테이너에서 특정 조건을 만족하는 자식을 확인할 수 있다.
+
+//input 태그의 invalid : input 태그아 invalid라면 링부분을 빨간색으로!(peer는 형제로 지정)
+//button 태그의 peer : 형제인 input 태그가 invalid라면 display: block으로 해당 글씨가 보이게 해줘
+//div 태그의 has-[:invalid]: 자식태그 중에서 invalid 상태라면 바탕색을 red-100으로 해줘.
+//has-[:invalid]:ring-red-100 : invalid인 자식이 있다면 ring-red-100을 적용하고 
