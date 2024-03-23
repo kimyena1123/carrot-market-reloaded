7266c77a3c759f51114be08f9edc1d82ffb509ba
kimyena1123 <kyq104@gmail.com>
2024-03-23 16:08:13 +0900

    animate

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,16 @@ export default function Home() {
             lg:bg-cyan-100
             xl:bg-orange-100
             2xl:bg-purple-100">
-        <div className="bg-white shadow-lg p-5 
-                        rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+        <div className="bg-white shadow-lg p-5
+                        rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
             {["Nico", "Me", "You", "Yourself"].map((person, index) =>
 
-            <div key={index} className="flex items-center gap-5 
-                                       border-b-2 pb-5 last:pb-0 last:border-0">
+            <div key={index} className="flex items-center gap-5 *:animate-pulse">
 
-                <div className="size-10 bg-blue-400 rounded-full"/>
-                <span className="text-lg font-medium">{person}</span>
-                <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
-                    <span>{index}</span>
-                </div>
-
+                <div className="size-10  bg-blue-400 rounded-full"/>
+                <div className="w-40 h-4 rounded-full bg-gray-400"/>
+                <div className="w-20 h-4 rounded-full bg-gray-400"/>
+                
             </div> )}
         </div>
       </main>
