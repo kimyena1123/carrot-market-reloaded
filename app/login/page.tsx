43fad9292d7d566d5a11380ec32bc2bac078a971
kimyena1123 <kyq104@gmail.com>
2024-03-24 12:55:16 +0900

    server action basic

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,14 @@
-"use client";
-
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function Login(){
-    const onClick = async () => {
-        const response = await fetch("/api/users", {
-            method: "POST",
-            body:JSON.stringify({
-                username: "nico",
-                password: '1234',
-            }),
-        });
-        console.log(await response.json()); //{username: 'nico', password: '1234'}
+    //Login 컴포넌트 안에서 onSubmit이라는 함수를 만들거다. 
+    async function handleForm(){
+        "use server";
+        console.log("i run in the server baby!");
     }
-
+    
     return(
         <div className="flex flex-col gap-10 py-6">
             <div className="flex flex-col gap-2 *:font-medium">
@@ -23,14 +16,14 @@ export default function Login(){
                 <h2 className="text-xl">Log in with email and password.</h2>
             </div>
 
-            <form className="flex flex-col gap-3">
+            <form action={handleForm}
+                className="flex flex-col gap-3">
                 {/* 페이지에 따라 input 태그가 많아질 수도, 적어질수도 있다. 직접 입력하기보다는 컴포넌트로 빼두는게 효율적임! */}
                 <FormInput type="email" placeholder="Email" required errors={[]}/>
                 <FormInput type="password" placeholder="Password" required errors={[]}/>
                 
+                <FormButton loading={false} text="Log in"></FormButton>
             </form>
-            {/* <FormButton loading={false} text="Log in"></FormButton> */}
-            <span onClick={onClick}><FormButton loading={false} text="Log in"></FormButton></span>
 
             <SocialLogin />
         </div>
