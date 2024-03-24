1db834cafdb0c7e5d6945aa6008ea7d0e8fe226a
kimyena1123 <kyq104@gmail.com>
2024-03-24 13:42:44 +0900

    useFormStatus()

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,11 +8,13 @@ export default function Login(){
     //FormData는 FormData constructor 내부에서 오는 거다.
     async function handleForm(data:FormData){
         "use server";
-        console.log("i run in the server baby!");
-        console.log("이메일>> ", data.get("email"));
-        console.log("비번 >> ", data.get("password"));
+
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        console.log("logged in!");
+        // console.log("이메일>> ", data.get("email"));
+        // console.log("비번 >> ", data.get("password"));
     }
-    
+
     return(
         <div className="flex flex-col gap-10 py-6">
             <div className="flex flex-col gap-2 *:font-medium">
@@ -20,13 +22,16 @@ export default function Login(){
                 <h2 className="text-xl">Log in with email and password.</h2>
             </div>
 
+            {/* 여기서 action={handleForm}은 server action을 적용 
+            여기 action이 pending 상태인지를 자동으로 알아낸다.로딩중인지, 작업이 끝났는지를 자동으로 알아낸다.
+            중요) 이 hook은 form의 자식요소에서 사용해야 한다. form과 같은 곳에서 사용할 수 없다.*/}
             <form action={handleForm}
                 className="flex flex-col gap-3">
                 {/* 페이지에 따라 input 태그가 많아질 수도, 적어질수도 있다. 직접 입력하기보다는 컴포넌트로 빼두는게 효율적임! */}
                 <FormInput type="email" name="email" placeholder="Email" required errors={[]}/>
                 <FormInput type="password" name="password" placeholder="Password" required errors={[]}/>
                 
-                <FormButton loading={false} text="Log in"></FormButton>
+                <FormButton text="Log in"></FormButton>
             </form>
 
             <SocialLogin />
--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -1,19 +1,35 @@
+// 우리가 useFormStatus hook을 사용하기 위해서는 이걸 client component로 바꿔야 한다.
+// 당연하다. client component는 interactive 하다.  그리고 우린 이 버튼을 interactive하게 만들었다. 
+
+// 이 버튼은 form이 pending될 때, 즉 action이 pending 될 때 비활성화될 테니까, 그러니까 이건 client component가 되어야 한다.
+"use client";
+
+import { useFormStatus } from "react-dom";
+
 interface FormButtonProps{
-    loading: boolean;
+    //이제 이 버튼에서 loading은 필요없다. 왜냐하면 이제는 hook이 form의 로딩상태를 알려줄 것이기 때문이다.
+    // loading: boolean;
     text:string;
 }
 
 
 //버튼은 로딩중일 때 비활성화 시켜줘야 할 수도 있기 때문에 disabled 설정
 //text에도 loading이 필요하다
-export default function FormButton({loading, text}:FormButtonProps){
+export default function FormButton({text}:FormButtonProps){
+
+    //pending : true or false
+    //버튼은 form이 pending 상태(true)라면 비활성화될 거다.
+    //이 hook은 자동으로 부모 form을 찾을 거다.=> 여기서 부모 form은? => 로그인 페이지의 form action={handleForm}부분임.
+    const {pending} = useFormStatus();
+
     return(
-        <button disabled={loading} 
+        <button disabled={pending} 
                 className="primary-btn h-10 
                             disabled:bg-neutral-400 
                             disabled:text-neutral-300
                             disabled:cursor-not-allowed">
-            {loading? "Loading..": text}
+            {pending? "Loading..": text}
+            {/* pending == true라면 '로딩중..'이라는 텍스트를 보여준다. */}
         </button>
     );
 }
