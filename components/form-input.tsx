e4191e3193ef13e2d00b202543389cb7509f7c8a
kimyena1123 <kyq104@gmail.com>
2024-04-07 20:30:15 +0900

    InputHTMLAttributes<HTMLInputElement>

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -4,6 +4,8 @@
 //에러 또한 외부에서 커스텀 할 수 있어야 한다.
 //이 컴포넌트는 다른 페이지에서도 쓰일 예정이기에, type의 종류가 달라질 수 있다!
 
+import { InputHTMLAttributes } from "react";
+
 interface FormInputProps{
         type:string;
         placeholder: string;
@@ -15,7 +17,10 @@ interface FormInputProps{
 
 //{type}, {placeholder}, {required}, {errer} 값들은 모두 props에서 가져와야 한다.
 //타입스크립트를 사용하고 있기에, FormInputProps라는 인터페이스를 만들 것이다.
-export default function FormInput({type, placeholder, required, errors=[], name}: FormInputProps){
+//InputHTMLAttributes<HTMLInputElement> : 이건 generic이라고 함-> FormInput 컴포넌트는 위에 있는 interface안에 있는 내용들을 props로 받는데 
+// 그것뿐만 아니라 input이 받을 수 있는 모든 attributes 또한 받을 수 있다고 선언한 것(예를 들어 type이 number이면 min, max attributes를 설정할 수 있도록!)
+//이렇게 되면 위에 interface에 있는 것들은 모두 지울 수 있다.
+export default function FormInput({type, placeholder, required, errors=[], name}: FormInputProps & InputHTMLAttributes<HTMLInputElement>){
     return(
         <div className="flex flex-col gap-2">
                 <input type={type} 
