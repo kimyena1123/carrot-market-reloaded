6473bae60cec9a5509a1a12ea101435a3ca08dfe
kimyena1123 <kyq104@gmail.com>
2024-04-10 23:09:27 +0900

    SMS verification 완료

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,15 +1,58 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import validator from "validator"; //처음에 에러 발생. 왜? validator가 typescript 라이브러리가 아니라서!
 import { z } from "zod";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+//refine(): 첫번째 인자 - 내가 refine하려는 data이다.  
+// const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+// const phoneSchema = z.string().trim().refine(phone => validator.isMobilePhone(phone));
+const phoneSchema = z.string().trim().refine(phone => validator.isMobilePhone(phone, "ko-KR"),"Wrong phone format");
 
 //tokenSchema는 number가 아니다.string으로 변환됨
 //coerce.number: 유저가 입력한 string을 number로 변환하려고 시도함
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogIn(prevState: any, formData: FormData){
-    console.log(typeof formData.get("token"));
-    console.log(typeof tokenSchema.parse(formData.get("token")))
+interface ActionState {
+    token:boolean
+}
+
+export async function smsLogIn(prevState: ActionState, formData: FormData){
+    // console.log(typeof formData.get("token")); //string
+    // console.log(typeof tokenSchema.parse(formData.get("token"))) //number
+
+    const phone = formData.get("phone");
+    const token = formData.get('token');
+
+    //token이 false이면 검증 코드를 받는 input 태그를 숨긴다는 의미
+    if(!prevState.token){ //prevState.token이 false이면 이 action을 처음 호출했다는 뜻이다. 즉, 유저가 전화번호만 입력했다는 의미
+        const result = phoneSchema.safeParse(phone);
+
+        //잘못된 전화번호를 입력하면 validation이 실패하고, token false를 return 한다는 의미.
+        if(!result.success){
+            console.log(result.error.flatten());
+
+            return{
+                token:false, //token이 true가 아니라면 유저가 다음 단계로 넘어갈 수 없도록 설정
+                error:result.error.flatten()
+            }
+        }else{
+            return{
+                token:true,
+            }
+        }
+    }else{ //token이 true일 때
+        const result = tokenSchema.safeParse(token);
+
+        if(!result.success){
+            return{
+                token:true,
+                error:result.error.flatten()
+
+            }
+        }else{
+            //sms 로그인 성공 -> redirect
+            redirect("/");
+        }
+    }
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,9 +5,14 @@ import Input from "@/components/input";
 import { useFormState } from "react-dom";
 import { smsLogIn } from "./actions";
 
+const initialState = {
+    token:false,
+    error:undefined,
+}
+
 export default function SMSLogin(){
     
-    const [state, dispatch] = useFormState(smsLogIn, null);
+    const [state, dispatch] = useFormState(smsLogIn, initialState);
 
     return(
         <div className="flex flex-col gap-10 py-6">
@@ -18,24 +23,26 @@ export default function SMSLogin(){
 
             <form action={dispatch} className="flex flex-col gap-3">
                 {/* 페이지에 따라 input 태그가 많아질 수도, 적어질수도 있다. 직접 입력하기보다는 컴포넌트로 빼두는게 효율적임! */}
-                <Input type="text" 
-                        name="phone" 
-                        placeholder="Phone number" 
-                        required 
-                />
 
                 {/* 인증할 때 사용할거임: 우리가 사용자에게 인증번호가 담긴 문자를 보내줄 것이다 */}
+                {state.token ? (
                 <Input type="number" 
                         name="token" 
                         placeholder="Verification code" 
                         required 
                         min={100000}
                         max={999999}
-                />
+                />) : 
+                (<Input type="text" 
+                    name="phone" 
+                    placeholder="Phone number" 
+                    required 
+                    errors={state.error?.formErrors}
+                />)}
                 
                 {/* 사용자가 이 페이지에 처음 왔을 때는 첫번째 input 태그만 보이며 버튼은 send the message라고 할 것이다
                  그 후 사용자에게 메시지가 도착하면 1,2번째 input 태그가 다 보이며 버튼은 Verify라는 문구로 변하게 할 것임. */}
-                <Button text="Verify"></Button>
+                <Button text={state.token ? "Verify Token" : "Send Verification SMS"}></Button>
 
             </form>
         </div>
