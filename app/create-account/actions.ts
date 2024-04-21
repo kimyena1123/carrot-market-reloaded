26ab98b6d3c42884516a849d6ebcf2ab1debcc79
kimyena01 <kimyena1123@nanugo.kr>
2024-04-21 16:19:22 +0900

    fix: password validation 함수화, username, email 존재여부 함수화 진행

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -6,6 +6,51 @@ import { z } from "zod";
 
 const checkUsername = (username:string) => !username.includes("미친");
 
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+  //1. username이 유일한지 체크 -> user가 존재한다면, 사용자에게 에러를 보낸다
+const checkUniqueUsername = async (username:string) => {
+  const user = await db.user.findUnique({
+    where:{
+      username,
+    },
+    select: {
+      id:true, 
+    }
+  });
+
+  //console.log(user); //존재한다면 : { id: 3 }, 존재하지 않는다면 : null(null이어야 한다)
+
+  // if(user){
+  //   return false;
+  // }else{
+  //   return true;
+  // }
+
+  return !Boolean(user); //user가 null이라면 false를 return
+}
+
+//2. email이 유일한지 체크
+//checkUniqueEmail에서 모든 것이 잘 될 경우 true를 return하고, 무언가 잘못되면 false를 return한다
+const checkUniqueEmail = async (email:string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return Boolean(user) === false; //user를 찾으면 true, null이라면 false.
+}
+
 
 const formSchema = z
   .object({
@@ -17,23 +62,26 @@ const formSchema = z
       .trim() //유저가 시작과 끝에 공백을 넣었을 때, string 앞뒤에 붙은 공백을 제거해준다.
       .toLowerCase() //유저가 대문자로 입력해도 소문자로 바꿔준다. 
       // .transform((username) => `🔥 ${username}`)
-      .refine(checkUsername, "미친은 사용x"), //"미친"이 포함되어 있다면 메시지 출력
-    email: z.string().email().toLowerCase(),
-    password: z
-      .string()
-      .min(PASSWORD_MIN_LENGTH),
-      // .regex(PASSWORD_REGEX,PASSWORD_REGEX_ERROR),
+      .refine(checkUsername, "미친은 사용x") //"미친"이 포함되어 있다면 메시지 출력
+      .refine(checkUniqueUsername, "해당 username은 이미 있는 이름입니다"),
+    email: z.string().email().toLowerCase().refine(checkUniqueEmail, "이미 존재하는 이메일입니다"),
+    password: z.string().min(PASSWORD_MIN_LENGTH),
+    //.regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
     confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
   })
-  .superRefine(({ password, confirm_password }, ctx) => {
-    if (password !== confirm_password) {
-      ctx.addIssue({
-        code: "custom",
-        message: "두 비밀번호가 일치해야 합니다!",
-        path: ["confirm_password"],
-      });
-    }
+  .refine(checkPasswords, {
+    message: "Both passwords should be the same!",
+    path: ["confirm_password"],
   });
+  // .superRefine(({ password, confirm_password }, ctx) => {
+  //   if (password !== confirm_password) {
+  //     ctx.addIssue({
+  //       code: "custom",
+  //       message: "두 비밀번호가 일치해야 합니다!",
+  //       path: ["confirm_password"],
+  //     });
+  //   }
+  // });
 
 
 
@@ -48,44 +96,13 @@ export async function createAccount(prevState: any, formData: FormData) {
     };
 
     //parse가 아닌 safeParse를 쓰면 try catch문을 안써도 된다
-    const result = formSchema.safeParse(data);
+    const result = await formSchema.safeParseAsync(data);
     //console.log(result); // //{ success: false, error: [Getter] }
 
     if (!result.success) {
         return result.error.flatten(); //이렇게 출력되는 결과가 state에 담기는 것임(useFormState)
     } else { //validation이 성공했다면
-      //사용자가 제출한 email과 username이 데이터베이스에 있는지 없는지 확인해야 한다(unique 하니까 - 고유해야 하니까!)
-      //다른 사람과 동일한 이메일이나 사용자명을 가질 수 없다 -> 이 두가지 검사가 false로 통과한다면 비밀번호를 해싱(hashing)해야 한다
 
-      //1. username이 유일한지 체크 -> user가 존재한다면, 사용자에게 에러를 보낸다
-      const user = await db.user.findUnique({
-        where:{
-          username: result.data.username
-        },
-        select: {
-          id:true, //데이터베이스에게 user를 찾지만, id만 달라고 하는 것임 왜? user만 존재하는지만 알면 되기 때문.
-        }
-      });
-
-      console.log(user); //존재한다면 : { id: 3 }, 존재하지 않는다면 : null
-
-      if(user){
-        //유저가 존재한다면 에러를 보내기
-      }
-
-      //2. email이 유일한지 체크
-      const userEmail = await db.user.findUnique({
-        where:{
-          email: result.data.email,
-        },
-        select:{
-          id: true,
-        }
-      });
-
-      if(userEmail){
-        //이미 존재하는 이메일이라면 에러 출력
-      }
 
       //3. 비밀번호 해싱(Hashing)
 
