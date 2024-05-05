c72e0fd9eb4346e496f11ef6d706d9319410ea4f
Kim Yena <kimyena@Kimui-MacBookAir.local>
2024-05-05 20:21:55 +0900

    feat/상대경로로 할 시 에러 발생, 절대경로로. 하지만 http://localhost3000을 쓸 수 없으니 request.url을 사용해서 구현

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,7 @@ import { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest){
     if(request.nextUrl.pathname === "/profile"){
-        return Response.json({
-            error: "you are not allowed here!",
-        });
+        //절대경로를 사용해야 함. 싱대경로x
+        return Response.redirect(new URL("/", request.url)); 
     }
 }
